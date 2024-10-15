1c973961d533769e6dba40e0616278836549f7fb
cbg <cbgianna@icloud.com>
2024-10-15 17:04:06 +0200

    consegna preliminare

--- sketch.js
+++ sketch.js
@@ -1,17 +1,59 @@
-function preload() {
-  // put preload code here
-}
+// Laboratorio di Computer Grafica per l'Information Design
+// Christian Battista Giannarelli
+// Vera Molnar, De La Serie (Des) Ordres (detail), 1974. Courtesy of The Anne and Michael Spalter Digital Art Collection.
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+	// Canvas grande quanto tutta la finestra
+	createCanvas(windowWidth, windowHeight);	
 }
 
 function draw() {
-  // put drawing code here
+	// La funzione draw() non viene ripetuta
+	noLoop();
+
+	// Numero di colonne
+	let cols = 8;
+	// Numero di righe
+	let rows = 8;
+
+	// Lato del quadrato
+	let squareSize = windowWidth / cols;
+
+	// Colonne
+	for (let i = 0; i < cols; i++) {
+		// Righe
+		for (let j = 0; j < rows; j++) {
+			// Calcolo della posizione orizzontale
+			let x = i * squareSize;
+			// Calcolo della posizione verticale
+			let y = j * squareSize;
+			
+			// Disegna quadrati concentrici con variazioni casuali
+			drawSquares(x, y, squareSize);
+		}
+	}
+}
+
+// Funzione per il disegno dei quadrati
+function drawSquares(x, y, size) {
+	// Numero casuale di quadrati interni
+	let layers = int(random(5, 15));
+	// Distanza casuale tra i quadrati
+	let spacing = random(5, 15);
+
+	// Nessun riempimento
+	noFill();
+
+	// Creazione dei quadrati interni
+	for (let i = 0; i < layers; i++) {
+		let currentSize = size - i * spacing;
+
+		// Disegno dei quadrati se 
+		if (currentSize > 0) {
+			// Spessore del contorno scelto casualmente
+			strokeWeight(random(1, 3));
+			// Creazione del quadrato (i quadrati sono centrati rispetto a quello iniziale)
+			rect(x + (size - currentSize) / 2, y + (size - currentSize) / 2, currentSize, currentSize);
+		}
+	}
 }
